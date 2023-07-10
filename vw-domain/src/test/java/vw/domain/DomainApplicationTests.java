fc0f6521f6a53169fc45dec379a307e119da4dc6
pocj8ur4in <pocj8ur4in@gmail.com>
2023-07-10 23:16:54 +0900

    init: 멀티모듈 프로젝트 세팅

---
+++ vw-domain/src/test/java/vw/domain/DomainApplicationTests.java
@@ -0,0 +1,11 @@
+package vw.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
