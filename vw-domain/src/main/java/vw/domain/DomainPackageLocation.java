b21e88c86f0b54352a94d629125f544464b54537
pocj8ur4in <pocj8ur4in@gmail.com>
2023-07-19 11:33:51 +0900

    init: JPA Auditing 추가

---
+++ vw-domain/src/main/java/vw/domain/DomainPackageLocation.java
@@ -0,0 +1,3 @@
+package vw.domain;
+
+public class DomainPackageLocation {}
---
+++ vw-domain/src/main/java/vw/domain/common/config/JpaConfig.java
@@ -0,0 +1,15 @@
+package vw.domain.common.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import vw.domain.DomainPackageLocation;
+
+@Configuration
+@EnableJpaAuditing
+@EntityScan(basePackageClasses = DomainPackageLocation.class) // // Entity 스캔을 위한 기본 패키지 위치를 설정
+@EnableJpaRepositories(
+        basePackageClasses = DomainPackageLocation.class) // JPA 저장소를 활성화하기 위한 기본 패키지 위치를 설정
+public class JpaConfig { // JPA Auditing 활성화
+}
